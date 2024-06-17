152477afc7dbfc172412014ee5ab031b3a99edd3
Danish Shafi <danish@equitas.ai>
2024-06-17 18:24:26 +0100

    changed tailwind.config file and added prettier es-lint

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ export default function Home() {
     <main>
       <div>
         <h1 className="text-xl">My Portfolio</h1>
-        <p className="text-7xl font-bold tracking-tighter">Hi, welcome to my portfolio.</p>
+        <p className="text-7xl font-bold tracking-tighter">
+          Hi, welcome to my portfolio.
+        </p>
       </div>
     </main>
   );
