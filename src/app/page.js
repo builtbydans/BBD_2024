e3b9c3d8fd1accaa022cafae15030e3fcce7409a
Danish Shafi <danish@equitas.ai>
2024-06-17 18:18:55 +0100

    basic nextjs skeleton created, src folder created, structure created, tailwind tested

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-xl">My Portfolio</h1>
+        <p className="text-7xl font-bold tracking-tighter">Hi, welcome to my portfolio.</p>
+      </div>
+    </main>
+  );
+}
